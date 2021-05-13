9c56ca4f5a6fd1e58c652c634128d11f6338ec20
Vitaly <vtl.stk@gmail.com>
2021-05-13 19:46:43 +0200

    Создал файл констант

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+const applicantForm = {
+  title: 'Анкета соискателя',
+  personalData: {
+    name: 'Личные данные',
+    inputs: ['Имя', 'Фамилия', 'Электронная почта'],
+    buttonName: 'Загрузить резюме'
+  },
+  gender: {
+    name: 'Пол',
+    inputs: ['Мужской', 'Женский'],
+  },
+  github: {
+    name: 'Github',
+    input: 'Вставьте ссылку на Github',
+  },
+  buttonName: 'Отправить'
+};
+
+export {
+  applicantForm,
+};
